1507da4be0f568e1b3011e6c78e4c4d32a525caf
BogBond2000 <111767325+BogBond2000@users.noreply.github.com>
2024-11-15 13:35:44 +0300

    Update dict.asm

--- IPL_2_LABA/dict.asm
+++ IPL_2_LABA/dict.asm
@@ -3,25 +3,27 @@ section .text
 find_word:
     push r15
     push r14
-    xor r14, r14
-    xor r15, r15
-    mov r14, rdi ; Сохраняем указатель на искомое слово
-.loop:
-    mov r15, rsi ; Сохраняем текущий адрес ключа
-    add rsi, 8   ; Переходим к адресу значения
-    mov rdi, r14 ; Восстанавливаем указатель на искомое слово
+    xor r15,r15
+    xor r14,r14
+    mov r14,rdi
+.loop
+    mov r15,rsi
+    add rsi,8
+    mov rdi,r14
     call string_equals
-    test rax, rax
-    jnz  .equal ; Если ключи равны, переходим к .equal
-    mov rsi, [r15] ; Загружаем следующий ключ
-    test rsi, rsi
-    jnz .loop ; Если следующий ключ не равен 0, продолжаем поиск
-    xor rax, rax ; Ключ не найден, возвращаем 0
+    test rax,rax
+
+    jnz .equal
+    mov rsi,[r15]
+    test rsi,rsi
+    jnz .loop
+    xor rax,rax
     jmp .end
 .equal:
-    add r15, 8 ; Переходим к адресу значения
-    mov rax, r15 ; Возвращаем адрес значения
+    mov rax,[rsi+16]
 .end:
     pop r14
     pop r15
     ret
+
+
